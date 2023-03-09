e6d134c079c6b8e7cb12de314d1e8b1e5585d3b9
N-HaoTien <N-HaoTien>
2023-03-09 22:48:41 +0700

    Create Project

---
+++ HumanResourcesManagement/HumanManagement/src/main/java/lth/com/hrm/HumanManagement/HumanManagementApplication.java
@@ -0,0 +1,13 @@
+package lth.com.hrm.HumanManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanManagementApplication.class, args);
+	}
+
+}
---
+++ HumanResourcesManagement/HumanManagement/src/test/java/lth/com/hrm/HumanManagement/HumanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lth.com.hrm.HumanManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
